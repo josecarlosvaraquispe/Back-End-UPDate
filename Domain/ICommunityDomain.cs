6fc3064715a84fc94d93ecfb346006af823696db
JosR <josueramos17@hotmail.com>
2023-05-30 17:09:57 -0500

    chore: Added Community to Domain section

---
+++ Domain/ICommunityDomain.cs
@@ -0,0 +1,13 @@
+﻿using Infraestructure.DataClass;
+using Infrastructure.Model;
+
+namespace Domain;
+
+public interface ICommunityDomain
+{
+    List<Community> GetAll();
+    public Community GetById(int id);
+    bool Create(CommunityData communityData); 
+    bool Update(int id, CommunityData communityData );
+    bool Delete(int id);
+}
